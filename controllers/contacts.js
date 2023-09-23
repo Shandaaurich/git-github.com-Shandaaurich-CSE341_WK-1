fbb22c707a5c68827127284e27c77ecbecc7971e
ShandaAurich <shanda@aurich.co>
2023-09-22 18:21:48 -0700

    routes.rest working with rest client

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,26 @@
 //connect to MongoDB
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 
-const getData = async (req, res) => {
+const getData = async (req, res, next) => {
     //wait for connection find()everything inside
     const result = await mongodb.getDb().db().collection('contacts').find();
     //return as an array and loop through
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0].contacts);
+        res.status(200).json(lists);
+    });
+};
+
+const getData1 = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
     });
 };
 
 //export the data from the db
-module.exports = { getData };
+module.exports = { getData, getData1 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -8,4 +8,6 @@ const router = express.Router();
 //if we hit this route: localhost:8080/contacts/, then call a funtion in the controller folder (contacts is the url)
 router.get('/', contacts.getData);
 
+router.get('/:id', contacts.getData1);
+
 module.exports = router;
